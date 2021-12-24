38ba9ed2d942126f716b448ddca38958daf9a610
JunYoungJee <rainmaker8492@gmail.com>
2021-12-24 16:47:30 +0900

    Setting Basic SwiftUI View For Testing

---
+++ TestBed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestBed
+//
+//  Created by sokol on 2021/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    init() {
+        print("ContentView init")
+    }
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
