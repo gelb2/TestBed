1226cedec8c8e703188700233f21d5f574135f65
JunYoungJee <rainmaker8492@gmail.com>
2022-05-15 22:40:12 +0900

    Handling User Input
    Section 1,2

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     
     var body: some View {
         NavigationView {
-            List(landmarks) { landMark in
+            List(filteredLandmarks) { landMark in
                 NavigationLink {
                     LandmarkDetail(landmark: landMark)
                 } label: {
@@ -25,10 +32,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone 13"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
     }
 }
