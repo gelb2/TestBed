72f87286a998427fc8042c32b2ef0f54bdc7176a
JunYoungJee <rainmaker8492@gmail.com>
2022-05-27 06:45:52 +0900

    Section4

--- Landmarks/Landmarks/Views/PageViewGroup/PageView.swift
+++ Landmarks/Landmarks/Views/PageViewGroup/PageView.swift
@@ -13,9 +13,11 @@ struct PageView<Page: View>: View {
     @State private var currentPage = 0
     
     var body: some View {
-        VStack {
+        ZStack(alignment: .bottomTrailing) {
             PageViewController(pages: pages, currentPage: $currentPage)
-            Text("current page : \(currentPage)")
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 10))
+                .padding(.trailing)
         }
         
     }
