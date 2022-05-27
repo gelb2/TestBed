44cb75aeaa23514e695838dd0997a6f8308a7fba
JunYoungJee <rainmaker8492@gmail.com>
2022-05-27 12:36:11 +0900

    Section 5 step 1
    Section 5

--- Landmarks/WatchLandmarks WatchKit Extension/NotificationView.swift
+++ Landmarks/WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct NotificationView: View {
+    
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if landmark != nil {
+                CircleImage(image: landmark!.image.resizable())
+                    .scaledToFit()
+            }
+            
+            Text(title ?? "unknown landmark")
+                .font(.headline)
+            
+            Divider()
+            
+            Text(message ?? "you are within 5miles of one of your favorite landmarks")
+                .font(.caption)
+        }
+        .lineLimit(0)
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        NotificationView()
+        Group {
+            NotificationView()
+            NotificationView(title: "turtle rock", message: "you are within 5miles of turtle rock", landmark:  ModelData().landmarks[0])
+        }
     }
 }
