ab674c50e1e7db271b53884299a7dac6da4e3693
JunYoungJee <rainmaker8492@gmail.com>
2022-05-28 10:14:48 +0900

    Section5

---
+++ Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,15 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by sokol on 2022/05/28.
+//
+
+import Foundation
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
