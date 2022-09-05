a8a1cafac181d6f936b5ef575dbab01aff460719
JunYoungJee <rainmaker8492@gmail.com>
2022-09-05 10:27:12 +0900

    주석 추가

--- TestBed/YT-Vapor-iOS-App-CodeWithChris/SpyAddUpdateSongViewModel.swift
+++ TestBed/YT-Vapor-iOS-App-CodeWithChris/SpyAddUpdateSongViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 @testable import TestBed
 
+//사실 AddUpdateSongViewModel 클래스를 직접 상속받는게 아니라 AddUpdateSongViewModelProtocol 같은 프로토콜을 채택하는 식으로 수정 하는 것이 맞는 걸로 보인다.
 final class SpyAddUpdateSongViewModel: AddUpdateSongViewModel {
     override func isValidSong() -> Bool {
         super.isValidSong()
